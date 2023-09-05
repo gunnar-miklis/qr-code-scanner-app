aeb5905e03133802d76dc85d534a050bcfad8ddb
Gunnar Miklis <gunnar.miklis@gmail.com>
2023-09-05 21:47:44 +0200

    add check-in time feature

--- mockDB/mockData.js
+++ mockDB/mockData.js
@@ -3,20 +3,24 @@ export const purchasedTickets = [
 		name: 'Anna',
 		ticketID: 'Vn64-7zqn-1Xj6-G1XX',
 		isCheckedIn: false,
+		checkInTime: null,
 	},
 	{
 		name: 'Scott',
 		ticketID: '2z1k-5E81-ox35-K6a4',
 		isCheckedIn: false,
+		checkInTime: null,
 	},
 	{
 		name: 'Julia',
 		ticketID: 'M0X9-3F5A-0etl-KL2M',
 		isCheckedIn: false,
+		checkInTime: null,
 	},
 	{
 		name: 'John',
 		ticketID: 'gea5-ujWn-Q933-n0Mq',
 		isCheckedIn: false,
+		checkInTime: null,
 	},
 ];
